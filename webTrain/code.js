f2ecd4fc17bc2b1097774ffc1d23f3d4355c89a6
SagiGalian99 <89161848+SagiGalian99@users.noreply.github.com>
2021-09-23 02:34:39 +0300

    Add files via upload

---
+++ webTrain/code.js
@@ -0,0 +1,37 @@
+function perfectCircle(){
+    document.getElementById("description").style.display = 'block';
+    document.getElementById("links").style.display = 'none';
+    document.getElementById("aboutme").style.display = 'none';
+    const label = "Perfect Circle";
+    const description = "Perfect circle is a website that check your drawing skills, you need to draw your best symmetrical circle around the mid point. you will get score by % on each paint"
+    document.getElementById("des-label").innerHTML = label;
+    document.getElementById("des-p").innerHTML = description;
+}
+function codeFaster(){
+    document.getElementById("description").style.display = 'block';
+    document.getElementById("links").style.display = 'none';
+    document.getElementById("aboutme").style.display = 'none';
+    const label = "Code Faster";
+    const description = "Code faster is a website that will make you improve your coding typing skills, in this website you can choose between 3 diffrent coding languages. in every language you have the main keywords in the selected language. as long as you play with it you will get better and better. your coding life will be much easier"
+    document.getElementById("des-label").innerHTML = label;
+    document.getElementById("des-p").innerHTML = description;
+}
+function rollete(){
+    document.getElementById("description").style.display = 'block';
+    document.getElementById("links").style.display = 'none';
+    document.getElementById("aboutme").style.display = 'none';
+    const label = "Rollete";
+    const description = "If you have to choose something or someone and you have no clue what to choose, this is the end of your problem, put the details and press the button, the luck will choose for you."
+    document.getElementById("des-label").innerHTML = label;
+    document.getElementById("des-p").innerHTML = description;
+}
+function normal(){
+    document.getElementById("links").style.display = 'block';
+    document.getElementById("description").style.display = 'none';
+    document.getElementById("aboutme").style.display = 'none';
+}
+function aboutMe(){
+    document.getElementById("links").style.display = 'none';
+    document.getElementById("description").style.display = 'none';
+    document.getElementById("aboutme").style.display = 'block';
+}
